33ba870a0f4ea3256c8e86ba703e68b16c694d4f
aidinjcs <aidin19_12@mail.ru>
2021-10-12 21:06:56 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+dateOfBirthday = 1998  # camel case
+date_of_birthday = 1998  # snake case
+
+var_int = 1  # Variable integer (число)
+
+var_str = "aidin"  # Variable string (строка)
+var_boolean_true = True  # variable boolean (истина)
+var_boolean_false = False  # variable boolean ( Лож)
+var_list = [1, 3, 4, "aidin", [1, 4, 5], (1, 4, 5), {}]  # variable list (список)
+var_tuple = (1, [1, 2, ], "aidin")
+var_dict = {"name": "aidin", "date_of_birthday": "01.01.2005", "course": 3,
+            "physical_data": [90, 180], "python": True}  # variable dictionary (словарь)
+
+
+def print_hi(var_list):
+    dateOfBirthday = 1998  # camel case
+    date_of_birthday = 1998  # snake case
+
+    var_list = 1  # Variable integer (число)
+
+    var_str = "aidin"  # Variable string (строка)
+    var_boolean_true = True  # variable boolean (истина)
+    var_boolean_false = False  # variable boolean ( Лож)
+    var_list = [1, 3, 4, "aidin", [1, 4, 5], (1, 4, 5), {}]  # variable list (список)
+    var_tuple = (1, [1, 2, ], "aidin")  # variable tuple (кортеж)
+    var_dict = {"name": "aidin", "date_of_birthday": "01.01.2005", "course": 3,
+                "physical_data": [90, 180], "python": True}  # variable dictionary (словарь)
+    print(var_list)
+
+
+
+
+if __name__ == '__main__':
+    print_hi(var_list)
